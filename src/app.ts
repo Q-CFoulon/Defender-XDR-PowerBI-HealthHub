662665e828a764961bc950dd441d53cd3d3306eb
Q.CFoulon <christoophe.foulon@quisitive.com>
2026-05-15 12:13:53 -0400

    feat: add scriptSrcAttr directive to content security policy for enhanced security

--- src/app.ts
+++ src/app.ts
@@ -37,6 +37,7 @@ export const createApplication = async (): Promise<ApplicationRuntime> => {
       directives: {
         defaultSrc: ["'self'"],
         scriptSrc: ["'self'", "'unsafe-inline'"],
+        scriptSrcAttr: ["'unsafe-inline'"],
         styleSrc: ["'self'", "'unsafe-inline'"],
         imgSrc: ["'self'", "data:"],
         connectSrc: ["'self'"]
